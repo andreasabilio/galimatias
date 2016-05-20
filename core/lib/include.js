3ca5b59518c8b1c2a0c03d3a65baeadcf2abe086
Andreas Abilio <andreas@abilio.dk>
2016-05-21 00:18:13 +0200

    include refactor

--- core/lib/include.js
+++ core/lib/include.js
@@ -9,6 +9,32 @@ var defaults = {
   filter:    /^([^\.].*)\.js(on)?$/
 };
 
+var walker = function(out, file){
+
+  // Get file path
+  var filepath = this.path + '/' + file;
+
+  if(fs.statSync(filepath).isDirectory()){
+
+    if( null === this.depth || 0 < this.depth){
+      var name = file.split('.')[0];
+      var conf = Object.assign({}, this);
+      conf.path  = filepath;
+      conf.depth = (this.depth)? this.depth - 1 : null;
+
+      out[name] = include(conf);
+    }
+
+  }else{
+    var match = file.match(this.filter);
+    if(match)
+      out[match[1]] = require(filepath);
+  }
+
+  return out;
+
+};
+
 
 var include = module.exports = function(_options){
 
@@ -24,44 +50,11 @@ var include = module.exports = function(_options){
   // Get the files in the path
   var files = fs.readdirSync(options.path);
 
-  // XXX
-  console.log(' ');
-  console.log('--- including folder:', options.path);
-  console.log('--- files:', files);
-  console.log('--- index:', -1 !== files.indexOf('index.js'));
-
   // Detect modules with index
   if( -1 !== files.indexOf('index.js') )
     return require(options.path + '/index.js');
 
 
   // Get files in path
-  return files.reduce(function(out, file){
-
-      // Get file path
-      var filepath = options.path + '/' + file;
-
-      if(fs.statSync(filepath).isDirectory()){
-
-        console.log('>>>', file);
-        console.log('>>>', options.depth);
-        console.log('--- descend:', null === options.depth || 0 < options.depth);
-
-        if( null === options.depth || 0 < options.depth){
-          var name = file.split('.')[0];
-          var conf = Object.assign({}, options);
-              conf.path  = filepath;
-              conf.depth = (options.depth)? options.depth - 1 : null;
-
-          out[name] = include(conf);
-        }
-
-      }else{
-        var match = file.match(options.filter);
-        if(match) out[match[1]] = require(filepath);
-      }
-
-      return out;
-
-    }, {});
+  return files.reduce(walker.bind(options), {});
 };
