5fec3b73fa30ffd6f37d39e7b6cff1961e82a27e
Andreas Abilio <andreas@abilio.dk>
2016-05-28 22:55:01 +0200

    refix

--- core/services/http/init.js
+++ core/services/http/init.js
@@ -3,7 +3,7 @@ module.exports = function(S){
 
   //// XXX
   //console.log(' ');
-  //console.log(' - Running Hosted service init');
+  //console.log(' - Running HTTP service init');
   ////console.log('    deps', Object.keys(this.manifest.dependencies));
   ////console.log(' - S', S);
 
